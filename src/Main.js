e391f2461147ff169e2d9b5e02ae6e15fd93d649
Trey Young(goodpudding) <james.trey.young@gmail.com>
2023-02-24 23:15:35 -0800

    added second animal and header and footer

--- src/Main.js
+++ src/Main.js
@@ -8,6 +8,10 @@ class Main extends React.Component{
       title = "Rhino" 
       imageUrl="https://upload.wikimedia.org/wikipedia/commons/4/4f/Rhino_%28234581759%29.jpeg" 
       description="A Large horned Animal"/>
+      <HornedBeast 
+      title = "Big Horned Sheep" 
+      imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzhtHoXwUfH9ZkILd3cHwjQsYW7bPsuV1EKFeGM9B9veIQjf-wCWZbP6sDbVhE7PyLEHE&usqp=CAU" 
+      description="A mountain sheep with large horns"/>
     </main>
     )
   }
